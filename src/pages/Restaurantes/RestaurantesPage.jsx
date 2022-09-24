dff74eedbbeeb273c3d831fc2b6e7a3dd9beedaa
mathroque <roque.mpr@gmail.com>
2022-09-24 15:45:50 -0300

    tela restaurantes fïx ID

--- src/pages/Restaurantes/RestaurantesPage.jsx
+++ src/pages/Restaurantes/RestaurantesPage.jsx
@@ -1,47 +1,31 @@
 import { Container, Typography, CircularProgress } from "@material-ui/core";
 import { useEffect, useState } from "react";
-import { getAllRestaurantes } from "../../services/restaurantes.service";
+import { getRestaurantes } from "../../services/restaurantes.service";
+import {  useParams } from "react-router-dom";
 import "./style.css";
 
 function RestaurantesPage() {
-  const [respostaApi, setRespostaApi] = useState([])
   const [nomeCategoria, setNomeCategoria] = useState([]);
-  const [restaurantesBaratinho, setRestaurantesBaratinho] = useState([]);
+  const [restaurantesBaratinho, setRestaurantesBaratinho] = useState();
   const [restaurantesNoPreco, setRestaurantesNoPreco] = useState([]);
   const [restaurantesCaro, setRestaurantesCaro] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const { id } = useParams();
+
   useEffect(() => {
-    getAllRestaurantes().then((response) => {
-      setRespostaApi(response)
-      
-      
-    })
+    getRestaurantes(id).then((response) => {
+      setNomeCategoria(response.categoria);
+      setRestaurantesBaratinho(response.baratinho);
+      setRestaurantesNoPreco(response.no_preco);
+      setRestaurantesCaro(response.caro);
+      setLoading(false);
+    });
   }, []);
-
-  const baratinho = respostaApi.map(item => {
-    return item.baratinho
-  })
-
-
-  const baratinhoItem = baratinho.map((item, i) => {
-    return item
-  })
-
-
-  // setNomeCategoria(response.categoria)
-  //     setRestaurantesBaratinho(response.baratinho);
-  //     setRestaurantesNoPreco(response.no_preco);
-  //     setRestaurantesCaro(response.caro);
-  //     setLoading(false);
-
-  const debug = () => {
-    console.log(baratinho);
-  }
 
   return (
     <Container className="restaurantes">
-      <Typography onClick={debug} variant="h5" align="center" color="primary" className="title">
+      <Typography variant="h5" align="center" color="primary" className="title">
         RESTAURANTES: {nomeCategoria}
       </Typography>
       {loading && (
@@ -54,33 +38,27 @@ function RestaurantesPage() {
           Baratinho <span>(</span>$ <span>$ $ $ $)</span>
         </Typography>
       </div>
-      {restaurantesBaratinho?.map(restaurante => (
-        <div key={restaurante.id}>
-          {restaurante.nome}
-        </div>
+      {restaurantesBaratinho?.map((restaurante) => (
+        <div key={restaurante.id}>{restaurante.nome}</div>
       ))}
       <div className="sub-header">
         <Typography variant="body1" color="primary">
           No preço <span>(</span>$ $ $ <span>$ $ )</span>
         </Typography>
       </div>
-      {restaurantesNoPreco?.map(restaurante => (
-        <div key={restaurante.id}>
-          {restaurante.nome}
-        </div>
+      {restaurantesNoPreco?.map((restaurante) => (
+        <div key={restaurante.id}>{restaurante.nome}</div>
       ))}
       <div className="sub-header">
         <Typography variant="body1" color="primary">
           Caro, mas vale a pena <span>(</span>$ $ $ $ $ <span> )</span>
         </Typography>
       </div>
-      {restaurantesCaro?.map(restaurante => (
-        <div key={restaurante.id}>
-          {restaurante.nome}
-        </div>
+      {restaurantesCaro?.map((restaurante) => (
+        <div key={restaurante.id}>{restaurante.nome}</div>
       ))}
     </Container>
-  )
+  );
 }
 
 export default RestaurantesPage;
